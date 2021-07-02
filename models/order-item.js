e3b5510d388aebb9f5268012fe625ae87726b492
EldarK-NS <eldark.ns@gmail.com>
2021-07-02 10:19:13 +0600

    02.07

--- models/order-item.js
+++ models/order-item.js
@@ -11,11 +11,4 @@ const orderItemSchema = mongoose.Schema({
   },
 });
 
-// change "_id" to "id"
-orderItemSchema.virtual("id").get(function () {
-  return this._id.toHexString();
-});
-orderItemSchema.set("toJSON", {
-  virtuals: true,
-});
 exports.OrderItem = mongoose.model("OrderItem", orderItemSchema);
